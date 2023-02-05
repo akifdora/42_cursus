4be1a8d475de9044e67d6768a5bf3f22fb515f13
Akif DORA <32612500+akifdora@users.noreply.github.com>
2023-02-05 04:48:36 +0300

    Add files via upload

---
+++ ft_printf/ft_printf.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdora <mdora@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/30 13:13:37 by mdora             #+#    #+#             */
+/*   Updated: 2023/02/04 11:12:04 by mdora            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putchar(char a)
+{
+	return (write(1, &a, 1));
+}
+
+int	ft_format(va_list *args, char c)
+{
+	if (c == 'u')
+		return (ft_unsigned(va_arg((*args), unsigned int)));
+	else if (c == 'c')
+		return (ft_putchar(va_arg((*args), int)));
+	else if (c == 'i' || c == 'd')
+		return (ft_int(va_arg((*args), int)));
+	else if (c == 'x' || c == 'X')
+		return (ft_hex(va_arg((*args), unsigned int), c));
+	else if (c == 'p')
+		return (ft_point(va_arg((*args), unsigned long), 1));
+	else if (c == 's')
+		return (ft_string(va_arg((*args), char *)));
+	else
+		return (ft_string("%"));
+}
+
+bool	ft_flag_catch(const char *str, int i)
+{
+	return (str[i] == '%' && (str[i + 1] == 'c' || str[i + 1] == 'd'
+			|| str[i + 1] == 'i' || str[i + 1] == 'u'
+			|| str[i + 1] == 'x' || str[i + 1] == 'X'
+			|| str[i + 1] == 'p' || str[i + 1] == 's' || str[i + 1] == '%'));
+}
+
+int	ft_printf(const char *str, ...)
+{
+	va_list	args;
+	int		i;
+	int		rtn;
+
+	i = -1;
+	rtn = 0;
+	va_start(args, str);
+	while (str[++i])
+	{
+		if (ft_flag_catch(str, i))
+			rtn += ft_format(&args, str[++i]);
+		else
+			rtn += write(1, &str[i], 1);
+	}
+	va_end(args);
+	return (rtn);
+}
---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdora <mdora@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/30 13:13:15 by mdora             #+#    #+#             */
+/*   Updated: 2023/01/30 13:13:15 by mdora            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdio.h>
+# include <unistd.h>
+# include <stdbool.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *str, ...);
+int	ft_format(va_list *args, char c);
+int	ft_int(int a);
+int	ft_hex(unsigned int a, char c);
+int	ft_point(unsigned long a, int sign);
+int	ft_string(char *str);
+int	ft_unsigned(unsigned int a);
+
+#endif
